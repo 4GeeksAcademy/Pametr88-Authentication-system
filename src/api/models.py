aded90261247804d436d59f8474e359a744d8c75
Pamela <140130365+Pametr88@users.noreply.github.com>
2023-11-30 03:03:45 +0000

    agregue modelos y rutas

--- src/api/models.py
+++ src/api/models.py
@@ -6,10 +6,12 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120), unique=True, nullable=False)
     user_name = db.Column(db.String(120), unique=True, nullable=False)
-    location = db.Column(db.String(120), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
+    location = db.Column(db.String(150), unique=True, nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(50), unique=False, nullable=False)
+    date_of_birth = db.Column(db.Date, unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    
 
     def __repr__(self):
         return f'<User {self.email}>'
@@ -17,6 +19,11 @@ class User(db.Model):
     def serialize(self):
         return {
             "id": self.id,
+            "name": self.name,
+            "user_name": self.user_name,
+            "location": self.location,
             "email": self.email,
+            "date_of_birth": self.date_of_birth.strftime('%Y-%m-%d %H:%M:%S'),
+            "is_active": self.is_active
             # do not serialize the password, its a security breach
         }
--- src/api/routes.py
+++ src/api/routes.py
@@ -5,18 +5,35 @@ from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
+import json
 
 api = Blueprint('api', __name__)
 
 # Allow CORS requests to this API
 CORS(api)
 
+@api.route("/sign_up", methods=['POST'])
+def create_User():
+    body = json.loads(request.data)
+    new_user = User(
+        name = body["name"],
+        user_name = body["user_name"],
+        location = body["location"],
+        email = body["email"],
+        password = body["password"],
+        date_of_birth = body["date_of_birth"],
+        is_active = True
 
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
+    )
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify ({"msg": "user created succesfull", "user_added": new_user}),200
 
-    response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
-    }
+@api.route('/user/<int:user_id>', methods=['GET'])
+def get_one_user(user_id):
+    user = User.query.get(user_id)
+    if user is None:
+        return jsonify({"msg": f"user with id {user_id} not found"}), 404
+    serialized_user = user.serialize()
+    return serialized_user, 200
 
-    return jsonify(response_body), 200
